c911000227a4796a839ec12a403c345c21fd548f
Karim Alomari <karim.alomari@gmail.com>
2020-07-23 19:22:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,8 +10,8 @@ end
 
 def countdown_with_sleep(number)
   while number > 0 do 
-  puts "#{number} SECOND(S)!"
-  sleep 1
-  number -= 1  
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1  
   end
 end
